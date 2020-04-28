b6adb849df90038dea830f40eba7d715f812a595
Elizabeth Munoz <munoz_714@live.com>
2020-04-27 18:12:03 -0700

    prompt add role set up

--- app.js
+++ app.js
@@ -24,17 +24,57 @@ const connection = mysql.createConnection({
 
 // Inquirer
 // function addDepartment() {
-inquirer
-    .prompt([
-        /* Pass your questions in here */
-        {
-            type: "input",
-            name: "department",
-            message: "Whats your Department?"
-        }
-    ])
-    .then(answers => {
-        console.log(answers);
-        connection.query('INSERT INTO department (name) VALUES (?)', [answers.department]);
+//     inquirer
+//         .prompt([
+//             /* Pass your questions in here */
+//             {
+//                 type: "input",
+//                 name: "department",
+//                 message: "Whats your Department?"
+//             }
+//         ])
+//         .then(answers => {
+//             console.log(answers);
+//             connection.query('INSERT INTO department (name) VALUES (?)', [answers.department]);
+//         });
+// };
+
+// function addRole() {
+
+connection.query('SELECT * FROM department', function (err, result) {
+    //new array for dept names
+    let deptNames = [];
+    //for loop thru depts
+    result.forEach(element => {
+        deptNames.push(element);
     });
+    inquirer
+        .prompt([
+            /* Pass your questions in here */
+            {
+                type: "input",
+                name: "title",
+                message: "Whats your Role title?"
+            },
+            {
+                type: "input",
+                name: "salary",
+                message: "Input your Salary"
+            },
+            {
+                type: "list",
+                name: "dept",
+                message: "Choose your dept",
+                choices: [deptNames]
+            }
+        ])
+        .then(answers => {
+            connection.query("SELECT department(id) FROM department WHERE name = ?"[answers.dept], (err, data) => {
+                const dept = data[0]['department(id)'];
+                connection.query("INSERT INTO role (departmentId) VALUES (?)", [dept]);
+            })
+            connection.query('INSERT INTO role (title , salary) VALUES (?)', [answers.title, answers.salary]);
+            console.log(answers);
+        });
+});
 // };
